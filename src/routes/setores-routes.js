9a453b07fdf9084ee77b49f760af95c91822aa8d
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-03-12 18:00:37 -0300

    Update setores-routes.js

--- src/routes/setores-routes.js
+++ src/routes/setores-routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const setorController = require ('../controllers/setores-controller.js');
 
 router.get('/setores', setorController.listarSetores);
+router.get('/setores/:id', setorController.listarSetorPorId);
+router.patch('/atualizarSetor/:id', setorController.atualizarSetor);
 
 module.exports = router;
