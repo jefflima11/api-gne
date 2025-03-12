3802eea06697dd75fa4673a18a79e0b768be1b73
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-03-12 18:00:33 -0300

    Update setores-controller.js

--- src/controllers/setores-controller.js
+++ src/controllers/setores-controller.js
@@ -10,4 +10,70 @@ async function listarSetores(req, res) {
     }
 };
 
-module.exports =  { listarSetores };
+async function listarSetorPorId(req, res) {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({ message: 'O ID do setor é obrigatório.' });
+    }
+
+    try {
+        const pool = await dbConnection;
+        const result = await pool.request()
+            .input('id', id)
+            .query(`SELECT 
+                        CD_SETOR,
+                        NM_SETOR,
+                        DT_CRIACAO,
+                        CASE
+                            WHEN DT_INATIVACAO IS NULL THEN 'S'
+                            ELSE 'N'
+                        END ATIVO
+                    FROM 
+                        dbo.setor 
+                    WHERE 
+                        cd_setor = @id
+                    `);
+
+        if (result.recordset.length === 0) {
+            return res.status(404).json({ message: 'Setor não encontrado.' });
+        }
+
+        res.status(200).json(result.recordset[0]);
+    } catch (error) {
+        console.error('Erro ao buscar setor:', error);
+        res.status(500).json({ message: 'Erro ao buscar setor.' });
+    }
+};
+
+async function atualizarSetor(req, res) {
+    const { id } = req.params;
+    const { nm_setor } = req.body;
+
+    if (!id) {
+        return res.status(400).json({ message: 'O ID do setor é obrigatório.' });
+    }
+
+    if (!nm_setor) {
+        return res.status(400).json({ message: 'O nome do setor é obrigatório.' });
+    }
+
+    try {
+        const pool = await dbConnection;
+        const result = await pool.request()
+            .input('id', id)
+            .input('nome', nm_setor)
+            .query('UPDATE dbo.setor SET nm_setor = @nome WHERE cd_setor = @id');
+
+        if (result.rowsAffected[0] === 0) {
+            return res.status(404).json({ message: 'Setor não encontrado.' });
+        }
+
+        res.status(200).json({ message: 'Setor atualizado com sucesso.' });
+    } catch (error) {
+        console.error('Erro ao atualizar setor:', error);
+        res.status(500).json({ message: 'Erro ao atualizar setor.' });
+    }
+};
+
+module.exports =  { listarSetores, atualizarSetor, listarSetorPorId };
