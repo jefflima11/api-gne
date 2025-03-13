ec0176052a446274844a122aae8c3c549d08ff7d
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-03-13 15:37:03 -0300

    Update setores-routes.js

--- src/routes/setores-routes.js
+++ src/routes/setores-routes.js
@@ -4,6 +4,9 @@ const setorController = require ('../controllers/setores-controller.js');
 
 router.get('/setores', setorController.listarSetores);
 router.get('/setores/:id', setorController.listarSetorPorId);
+router.post('/setores/add', setorController.adicionarSetor);
 router.patch('/atualizarSetor/:id', setorController.atualizarSetor);
+router.patch('/setores/edit/:id', setorController.editarAtividadeDoSetor);
+router.patch('/deletarSetores/:id', setorController.deletarSetores);
 
 module.exports = router;
