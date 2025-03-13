bf941807dd9df471d7fca33a326c2c55c6526901
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2025-03-13 17:17:40 -0300

    Update usuario-routes.js

--- src/routes/usuario-routes.js
+++ src/routes/usuario-routes.js
@@ -4,6 +4,7 @@ const usuarioController = require('../controllers/usuario-controller');
 
 router.get('/perfil', usuarioController.listarUsuarios);
 router.get('/perfil/:id', usuarioController.listarUsuarioPorId);
+router.post('/perfil/', usuarioController.inserirUsuario);
 router.patch('/atualizaPerfil/:id', usuarioController.atualizarUsuario);
 
 module.exports = router;
